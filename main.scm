3158c40c81b6d3185fec50635ea5772648bcbb35
Akifumi Fukuda <a.fukuda@mtl.t.u-tokyo.ac.jp>
2020-03-08 13:20:19 +0900

    Add tests

--- main.scm
+++ main.scm
@@ -1,23 +1,39 @@
-(define (new-env) '(()))
-(define (new-sub-env env) (cons '() env))
-(define (env-add env sym exp)
-  (let ((old (car env))
-	(parents (cdr env)))
-    (cons (cons (cons sym exp) old) parents)))
+; utils
+(define (find pred lis)
+  (if (null? lis)
+      #f
+      (if (pred (car lis))
+	  (car lis)
+	  (find pred (cdr lis)))))
 
-(define (env-find env sym)
-  (define (sub-env-find env sym)
-    (if (nil? env)
-	#f
-	(if (eq? sym (caar env))
-	    (cdar env)
-	    (env-find (cdr env) sym))))
-  
+(define (make-env parents)
+  (define env '())
+  (define (find-pred sym) (lambda (x) (eq? sym (car x))))
+  (define (add sym val)
+    (set! env (cons (cons sym val) env)))
+  (define (env-find sym)
+    (let ((result (find (find-pred sym) env)))
+      (if result
+	  result
+	  (if parents (parents 'find sym) #f))))
+  (define (update sym val)
+    (let ((find-result (find (find-pred sym) env)))
+      (if find-result
+	  (set! env (cons (cons sym val) env))
+	  (if parents (parents 'update sym val) #f))))
+    
+  (define (dispatch msg . args)
+    (cond
+     ((eq? msg 'add) (apply add args))
+     ((eq? msg 'find) (apply env-find args))
+     ((eq? msg 'update) (apply update args))
+     (else (format #t "failed to dispatch: env, msg(~a)\n" msg))))
+  dispatch)
 
 (define (eval env exp)
-  ; (format #t "eval called with env[~a] and exp[~a]\n" env exp)
+;  (format #t "eval called with env[~a] and exp[~a]\n" env exp)
   (cond
-   ((not (list? exp)) (cons env exp))
+   ((not (list? exp)) exp)
    ((eq? (car exp) 'write)
     (if (null? (cddr exp))
 	(write (eval env (cadr exp)))
@@ -28,9 +44,13 @@
       (cons env '())))))
 
 (define (main args)
-  (let ((port (open-input-file (cadr args))))
-    (let loop ((exp (read port)) (env (new-env)))
-      (if (eof-object? exp)
-	  '()
-	  (loop (read port) (car (eval env exp)))))))
+  (define (top-level (make-env #f)))
+  (if (null? (cdr args))
+      '() ; テストのために引数なしのときは何もせずに終わる
+      (let ((port (open-input-file (cadr args))))
+	(let loop ((exp (read port)))
+	  (eval top-level exp)
+	  (if (eof-object? exp)
+	      '()
+	      (loop (read port)))))))
 
